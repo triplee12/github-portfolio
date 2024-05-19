b0785eb649a838159cdf1f094723b641cc505ad5
triplee12 <tripleeoliver@gmail.com>
2024-05-19 09:52:57 +0100

    Implement Layout and Navbar components

---
+++ portfolio/src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Layout = ({ children }) => {
+    return (
+        <div>
+        <Navbar />
+        <main className="container mx-auto p-4">
+            {children}
+        </main>
+        </div>
+    );
+}
+
+export default Layout;
---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="bg-blue-500 p-4 text-white">
+        <Link to="/" className="mr-4">Home</Link>
+        </nav>
+    );
+}
+
+export default Navbar;
