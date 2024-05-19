de38b109a99003cc9ac22e0635addee02dc8fe27
triplee12 <tripleeoliver@gmail.com>
2024-05-19 10:26:58 +0100

    Fixed route error

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Repository from './pages/Repository';
@@ -12,11 +12,11 @@ const App = () => {
     <Router>
       <ErrorBoundary>
         <Layout>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/repo/:repoName" component={Repository} />
-            <Route component={NotFound} />
-          </Switch>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/repo/:repoName" element={<Repository />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </Layout>
       </ErrorBoundary>
     </Router>
--- portfolio/src/pages/Repository.jsx
+++ portfolio/src/pages/Repository.jsx
@@ -1,4 +1,3 @@
-// src/pages/Repository.jsx
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
